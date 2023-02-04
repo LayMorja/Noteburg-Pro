8f7396e7cfe8aefac037bee503cfbc4395e4709f
LayMorja <evil-corporaty@yandex.ru>
2023-02-04 10:18:11 +0300

    first commit

---
+++ config/settings/ftp.js
@@ -0,0 +1,6 @@
+export const ftpSetting = {
+	host: '37.140.192.135',
+	user: 'u1665794',
+	password: 'mF5dS8cH6lfN4qY4',
+	parallel: 10,
+};
---
+++ config/tasks/ftp.js
@@ -0,0 +1,28 @@
+import util from 'gulp-util';
+import vinylFTP from 'vinyl-ftp';
+import { ftpSetting } from '../settings/ftp.js';
+
+export function ftp() {
+	ftpSetting.log = util.log;
+	const connection = vinylFTP.create(ftpSetting);
+	return app.gulp
+		.src(
+			process.argv.includes('--full')
+				? [`${app.path.buildFolder}/**/*.*`, `!${app.path.build.js}/*.txt`]
+				: [
+						`${app.path.buildFolder}/**/*.*`,
+						`!${app.path.buildFolder}/phpmailer/*.*`,
+						`!${app.path.buildFolder}/*.php`,
+						`!${app.path.build.js}/*.txt`,
+				  ]
+		)
+		.pipe(
+			app.plugins.plumber(
+				app.plugins.notify.onError({
+					title: 'FTP',
+					message: 'Error: <%= error.message %>',
+				})
+			)
+		)
+		.pipe(connection.dest(`/${app.path.ftp}/${app.path.rootFolder.toLowerCase()}`));
+}
